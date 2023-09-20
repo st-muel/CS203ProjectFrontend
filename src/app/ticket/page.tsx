d8c05618afe7105938cdf0ecc3f91cc74ba75d2d
Zac4128 <yuchen1310@gmail.com>
2023-09-20 16:54:59 +0800

    feat: seatmap hover selection done

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -12,13 +12,21 @@ export default function Home() {
       <div className="bg-primary-black overflow-hidden">
         <Navbar />
         <div>
-          <ChooseSections />
-          <div className="gradient-03 z-0" />
-          <Seatmap />
+          {/* <ChooseSections /> */}
+          {/* <div className="gradient-03 z-0" /> */}
+          <div className="flex justify-center items-center h-screen bg-white">
+            <Seatmap />
+          </div>
+          <div className="">
+            <div className="grid grid-cols-2 gap-4">
+              <div>01</div>
+              <div>02</div>
+              <div>03</div>
+              <div>04</div>
+            </div>
+          </div>
         </div>
-        {/* <Seatmap /> */}
-        {/* <SeatSelector /> */}
-        <div className="gradient-04 z-0" />
+        {/* <div className="gradient-04 z-0" /> */}
         <Footer />
       </div>
     </main>
