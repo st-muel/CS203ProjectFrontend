7d5a7a75a6d469af04b9b3c2bf1f861e39275649
suo-haoran <suohaoran99@gmail.com>
2023-11-04 00:01:04 +0800

    fix: card width > parent div

--- src/app/components/ExploreCard.tsx
+++ src/app/components/ExploreCard.tsx
@@ -46,7 +46,7 @@ const ExploreCard = ({
       variants={fadeIn("right", "spring", index * 0.5, 1.75)}
       className="flex md:flex-row flex-col gap-2"
     >
-      <div className="rounded overflow-hidden shadow-lg">
+      <div className="rounded overflow-hidden shadow-lg w-96">
         <div className="relative h-48 w-96 bg-stone-600">
           <Image
             className="md:object-scale-down"
@@ -58,7 +58,7 @@ const ExploreCard = ({
         </div>
 
         <div className="px-6 py-4 bg-stone-900/50">
-          <div className="font-bold text-xl mb-2">{title}</div>
+          <div className="font-bold text-xl mb-2 break-normal">{title}</div>
           <p className="text-gray-500 text-base">{loc}</p>
           <p className="text-gray-500 text-base">
             {new Date(startDate).toDateString() +
