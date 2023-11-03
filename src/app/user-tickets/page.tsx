fa7b4addb747a6b6bdf75a1ad4857add34748666
suo-haoran <suohaoran99@gmail.com>
2023-11-04 00:44:46 +0800

    feat: added ticket purchased skeleton

--- src/app/user-tickets/page.tsx
+++ src/app/user-tickets/page.tsx
@@ -4,6 +4,12 @@ import { useAtom, useAtomValue } from "jotai";
 import { jwtTokenAtom } from "../jotai";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
+import { notification } from "antd";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { motion } from "framer-motion";
+import { TitleText, TypingText } from "../components/CustomText";
+import { staggerContainer } from "../utils/motion";
 
 export interface Ticket {
   id: string;
@@ -65,14 +71,59 @@ export default function UserTickets() {
             },
           }
         );
+        console.log(res.data);
         setTickets(res.data as Ticket[]);
       } catch (e) {
         console.log(e);
+        notification.error({ message: "Failed to fetch tickets" });
         return [];
       }
     };
     if (jwtToken) fetchData();
   }, [jwtToken]);
 
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <Navbar />
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className="mx-auto flex flex-col"
+      >
+        <TypingText title="| Tickets" textStyles="text-center" />
+        <TitleText title={<>Tickets Purchased</>} textStyles="text-center" />
+        <div className="flex justify-center">
+          {tickets.map((ticket) => {
+            return (
+              <a
+                href="#"
+                className="m-10 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+              >
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {ticket.concertSession.concert.title}
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Venue: {ticket.concertSession.concert.venue.name}
+                </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Date:{" "}
+                  {new Date(ticket.concertSession.datetime).toDateString()}
+                </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400 capitalize">
+                  Section: {ticket.seat.section.name.replace("_", "-")}
+                </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400 capitalize">
+                  Seat: {ticket.seat.seatRow}
+                  {ticket.seat.seatNumber}
+                </p>
+              </a>
+            );
+          })}
+        </div>
+      </motion.div>
+      <Footer />
+    </>
+  );
 }
