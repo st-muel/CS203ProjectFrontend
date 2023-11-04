45c30e7ebb98ad406974065c54b8525f2c1ff6e5
Zac4128 <yuchen1310@gmail.com>
2023-11-04 17:55:48 +0800

    update: ticket ui done

--- src/app/user-tickets/page.tsx
+++ src/app/user-tickets/page.tsx
@@ -10,6 +10,8 @@ import Footer from "../components/Footer";
 import { motion } from "framer-motion";
 import { TitleText, TypingText } from "../components/CustomText";
 import { staggerContainer } from "../utils/motion";
+import styles from "../styles";
+import Image from "next/image";
 
 export interface Ticket {
   id: string;
@@ -83,7 +85,7 @@ export default function UserTickets() {
   }, [jwtToken]);
 
   return (
-    <>
+    <div className="bg-primary-black overflow-hidden">
       <Navbar />
       <motion.div
         variants={staggerContainer}
@@ -94,36 +96,132 @@ export default function UserTickets() {
       >
         <TypingText title="| Tickets" textStyles="text-center" />
         <TitleText title={<>Tickets Purchased</>} textStyles="text-center" />
-        <div className="flex justify-center">
+        <div
+          className={`${styles.innerWidth} mx-auto flex items-center flex-col`}
+        >
           {tickets.map((ticket) => {
             return (
-              <a
-                href="#"
-                className="m-10 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-              >
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {ticket.concertSession.concert.title}
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  Venue: {ticket.concertSession.concert.venue.name}
-                </p>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  Date:{" "}
-                  {new Date(ticket.concertSession.datetime).toDateString()}
-                </p>
-                <p className="font-normal text-gray-700 dark:text-gray-400 capitalize">
-                  Section: {ticket.seat.section.name.replace("_", "-")}
-                </p>
-                <p className="font-normal text-gray-700 dark:text-gray-400 capitalize">
-                  Seat: {ticket.seat.seatRow}
-                  {ticket.seat.seatNumber}
-                </p>
+              // <a
+              //   href="#"
+              //   className="m-10 block max-w-sm bg-white border border-gray-200 w-96 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+              // >
+              //   <div className="flex flex-row items-center justify-center">
+              //     <div className="relative h-48 w-96 bg-stone-600">
+              //       <Image
+              //         className="md:object-scale-down"
+              //         src={
+              //           "/" +
+              //           ticket.concertSession.concert.concertImages[0].filePath
+              //         }
+              //         alt=""
+              //         objectFit="contain"
+              //         fill
+              //       />
+              //     </div>
+              //     <div>
+              //       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              //         {ticket.concertSession.concert.title}
+              //       </h5>
+              //       <p className="font-normal text-gray-700 dark:text-gray-400">
+              //         Venue: {ticket.concertSession.concert.venue.name}
+              //       </p>
+              //       <p className="font-normal text-gray-700 dark:text-gray-400">
+              //         Date:{" "}
+              //         {new Date(ticket.concertSession.datetime).toDateString()}
+              //       </p>
+              //       <p className="font-normal text-gray-700 dark:text-gray-400 capitalize">
+              //         Section: {ticket.seat.section.name.replace("_", "-")}
+              //       </p>
+              //       <p className="font-normal text-gray-700 dark:text-gray-400 capitalize">
+              //         Seat: {ticket.seat.seatRow}
+              //         {ticket.seat.seatNumber}
+              //       </p>
+              //     </div>
+              //   </div>
+              // </a>
+              <a href="#" className="w-screen flex flex-col">
+                <section className="w-full flex-grow flex items-center justify-center p-4 m-8">
+                  <div className="flex w-full max-w-3xl text-zinc-50 h-64">
+                    <div className="h-full bg-zinc-900 flex items-center justify-center px-8 rounded-l-3xl">
+                      <Image
+                        width={140}
+                        height={140}
+                        className="md:object-scale-down"
+                        src={
+                          "/" +
+                          ticket.concertSession.concert.concertImages[0]
+                            .filePath
+                        }
+                        alt=""
+                        objectFit="contain"
+                      />
+                    </div>
+                    <div className="relative h-full flex flex-col items-center border-dashed justify-between border-2 bg-zinc-900 border-zinc-50"></div>
+                    {/* color */}
+                    <div className="h-full py-8 px-10 bg-zinc-900 flex-grow rounded-r-3xl flex flex-col">
+                      <div className="flex w-full justify-between items-center">
+                        {/* <div className="flex flex-col items-center">
+                          <span className="text-4xl font-bold">BNE</span>
+                          <span className="text-zinc-500 text-sm">Brisbane</span>
+                        </div>
+                        <div className="flex flex-col flex-grow items-center px-10">
+                          <span className="font-bold text-xs">RS 11</span>
+                          <div className="w-full flex items-center mt-2">
+                            <div className="w-3 h-3 rounded-full border-2 border-zinc-900"></div>
+                            <div className="flex-grow border-t-2 border-zinc-400 border-dotted h-px"></div>
+                            <div className="flex-grow border-t-2 border-zinc-400 border-dotted h-px"></div>
+                            <div className="w-3 h-3 rounded-full border-2 border-zinc-900"></div>
+                          </div>
+                          <div className="flex items-center px-3 rounded-full bg-lime-400 h-8 mt-2">
+                            <span className="text-sm text-zinc-900">18h 35m</span>
+                          </div>
+                        </div>
+                        <div className="flex flex-col items-center">
+                          <span className="text-4xl font-bold">ATH</span>
+                          <span className="text-zinc-500 text-sm">Athens</span>
+                        </div> */}
+                        <span className="text-4xl font-bold text-pink-400 font-mono">
+                          {ticket.concertSession.concert.title}
+                        </span>
+                      </div>
+                      <div className="flex w-full mt-auto justify-between">
+                        <div className="flex flex-col">
+                          <span className="text-xs text-zinc-400">Date</span>
+                          <span className="font-mono">
+                            {new Date(
+                              ticket.concertSession.datetime
+                            ).toDateString()}
+                          </span>
+                        </div>
+                        <div className="flex flex-col">
+                          <span className="text-xs text-zinc-400">Venue</span>
+                          <span className="font-mono">
+                            {ticket.concertSession.concert.venue.name}
+                          </span>
+                        </div>
+                        <div className="flex flex-col">
+                          <span className="text-xs text-zinc-400">Section</span>
+                          <span className="font-mono">
+                            {ticket.seat.section.name.replace("_", "-")}
+                          </span>
+                        </div>
+                        <div className="flex flex-col">
+                          <span className="text-xs text-zinc-400">Seat</span>
+                          <span className="font-mono">
+                            {ticket.seat.seatRow}
+                            {ticket.seat.seatNumber}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
               </a>
             );
           })}
         </div>
       </motion.div>
       <Footer />
-    </>
+    </div>
   );
 }
